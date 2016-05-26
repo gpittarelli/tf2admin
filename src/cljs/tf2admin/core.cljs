72e4e5c0b01ccf4221fea80880256dc51872cc9b
George Pittarelli <g@gjp.cc>
2016-05-25 23:56:21 -0700

    shove some code together
    we don't need so much separation yet for such a small project

--- src/cljs/tf2admin/core.cljs
+++ src/cljs/tf2admin/core.cljs
@@ -1,20 +1,45 @@
 (ns tf2admin.core
-    (:require [reagent.core :as reagent]
-              [re-frame.core :as re-frame]
-              [tf2admin.handlers]
-              [tf2admin.subs]
-              [tf2admin.routes :as routes]
-              [tf2admin.views :as views]
-              [tf2admin.config :as config]))
+  (:require-macros [reagent.ratom :refer [reaction]])
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as re-frame]
+            [tf2admin.routes :as routes]
+            [tf2admin.views :as views]
+            [tf2admin.config :as config]))
+
+(def default-db
+  {:name "re-frame"})
+(re-frame/register-handler :initialize-db (fn  [_ _] default-db))
+
+(re-frame/register-handler
+ :send-command
+ (fn [db [_ active-panel]]
+   (assoc db :active-panel active-panel)))
+
+(re-frame/register-handler
+ :set-active-panel
+ (fn [db [_ active-panel]]
+   (assoc db :active-panel active-panel)))
+
+;; Subscriptions
+(re-frame/register-sub
+ :name
+ (fn [db]
+   (reaction (:name @db))))
+
+(re-frame/register-sub
+ :active-panel
+ (fn [db _]
+   (reaction (:active-panel @db))))
+
 
 (when config/debug?
   (println "dev mode"))
 
 (defn mount-root []
   (reagent/render [views/main-panel]
-                  (.getElementById js/document "app")))
+    (.getElementById js/document "app")))
 
-(defn ^:export init [] 
+(defn ^:export init []
   (routes/app-routes)
   (re-frame/dispatch-sync [:initialize-db])
   (mount-root))
--- src/cljs/tf2admin/db.cljs
+++
@@ -1,4 +0,0 @@
-(ns tf2admin.db)
-
-(def default-db
-  {:name "re-frame"})
