ee6e3d6cd5ecda71cf42cec97507837a82bb5469
George Pittarelli <g@gjp.cc>
2016-05-25 23:55:45 -0700

    Add clj-rcon dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject tf2admin "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
+                 [clj-rcon "0.1.0"]
                  [reagent "0.5.1"]
                  [re-frame "0.7.0"]
                  [secretary "1.2.3"]
