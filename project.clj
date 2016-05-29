ca814b7ac1aa0d53d162bde52c62b7b6b3b7be04
George Pittarelli <g@gjp.cc>
2016-05-28 22:51:50 -0700

    add sente ws library

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [garden "1.3.2"]
                  [http-kit "2.2.0-alpha1"]
                  [compojure "1.5.0"]
+                 [com.taoensso/sente "1.8.1"]
                  [ring "1.4.0"]
                  ]
 
