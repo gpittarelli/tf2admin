aee118d566cb8ffb46ff77510c405a3a3ae27a63
George Pittarelli <g@gjp.cc>
2016-05-29 20:04:07 -0700

    fix terminal to actually use enterd value

--- src/cljs/tf2admin/components/terminal.cljs
+++ src/cljs/tf2admin/components/terminal.cljs
@@ -1,8 +1,8 @@
 (ns tf2admin.components.terminal
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [cljs.core.async :as async :refer [<! chan put! timeout]]
-            [garden.core :refer [css]]
-            [re-frame.core :refer [dispatch]]))
+            [reagent.core :as r]
+            [garden.core :refer [css]]))
 
 (def initial-state "")
 
@@ -11,16 +11,19 @@
 (defmethod autocomplete :send [state [_ f]] (if (fn? f) (f state)) initial-state)
 
 (defn terminal [scrollback on-command]
-  (let [cmd-chan (chan)]
+  (let [cmd-chan (chan)
+        command (r/atom "")]
 
     (go-loop [state initial-state]
       (->> (<! cmd-chan)
-           (autocomplete val)
+           (autocomplete 1)
+           (reset! command)
            recur))
 
     (fn [scrollback on-command]
       [:div.terminal
        [:div.scrollback scrollback]
        [:input {:type "text"
+                :value @command
                 :on-change #(put! cmd-chan [:update (-> % .-target .-value)])}]
        [:button {:on-click #(put! cmd-chan [:send on-command])} "Send"]])))
