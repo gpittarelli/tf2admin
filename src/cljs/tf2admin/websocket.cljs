c7963b57800114918826f34a99335f0a397c1e23
George Pittarelli <g@gjp.cc>
2016-05-29 20:04:38 -0700

    make persistent websockets work

--- src/cljs/tf2admin/websocket.cljs
+++ src/cljs/tf2admin/websocket.cljs
@@ -1,9 +1,9 @@
 (ns tf2admin.websocket
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require [cljs.core.async :as async :refer [<! chan put! timeout]]
+  (:require [cljs.core.async :as async :refer [<! chan put! timeout mix mult tap]]
             [taoensso.sente :as sente :refer [cb-success?]]))
 
-(def all-conns (atom []))
+(def all-conns (atom {}))
 
 ;;(go-loop []
 ;;  (when (pos? (count @all-conns))
@@ -12,16 +12,26 @@
 ;;
 ;;  (recur))
 
-(defn listen [addr]
-  (let [new-listener (chan)
-        conn (get
-              (swap! all-conns
-                     update
-                     addr
-                     (fn [previous-conn]
-                       (if previous-conn
-                         previous-conn
-                         (sente/make-channel-socket! addr {:type :auto}))))
-              addr)]
-    (println conn)
-    new-listener))
+(defn- get-or-make-connection! [path opts]
+  (-> all-conns
+      (swap! update path
+             (fn [previous-conn]
+               (if previous-conn
+                 previous-conn
+                 (update (sente/make-channel-socket! path opts)
+                         :ch-recv
+                         mult))))
+      (get path)))
+
+(defn listen
+  ([path] (listen path {}))
+  ([path opts]
+   (let [new-listener (chan)
+         conn (get-or-make-connection! path (assoc opts :type :auto))]
+     (println "conn to " path opts)
+     (tap (:ch-recv conn) new-listener)
+     new-listener)))
+
+(defn send [path m]
+  (println "send" m "to" path)
+  ((:send-fn (get @all-conns path)) m))
